d98eabd543ab4bc59684067f9b08744ab46b845d
kosshishub <kosshishub@users.noreply.github.com>
2018-02-10 20:24:38 +0200

    Thumb icon is back, rewrote downloading code
    Re-implemented pull #2 in a simpler manner (requested eg. in #10). Less fancy but gets the job done, and is easier to maintain.
    Added progress to both ways of download.
    
    Rewrote downloading code. Main changes:
    - The files will be downloaded to memory first instead of directly piping to disk. This won't leave broken files if something goes wrong
    - Errors out if status code is not 200 (it just downloaded the error page before). Doesn't support redirecting?
    - Errors out if content-type header isn't an image of some kind.
    - Separated from DOM code
    - Callbacks for tracking progress
    
    A lot changed, there will be bugs.

--- Quicksave.plugin.js
+++ Quicksave.plugin.js
@@ -12,7 +12,7 @@ Quicksave.prototype.getDescription = function () {
 	return "Lets you save images fast.";
 };
 Quicksave.prototype.getVersion = function () {
-	return "0.2.0";
+	return "0.2.1";
 };
 
 
@@ -22,6 +22,38 @@ Quicksave.prototype.onMessage = function () {};
 Quicksave.prototype.onSwitch = function () {};
 
 Quicksave.prototype.start = function () {
+
+	BdApi.injectCSS("quicksave-style", `
+		.thumbQuicksave {
+
+			z-index: 9000!important;
+
+			background-color: rgba(51, 51, 51, .8);
+
+			position: absolute;
+			display: block;
+
+			padding: 3px 9px;
+			margin: 5px;
+
+			border-radius: 3px;
+
+		    font-family: inherit;
+		    color: #FFF;
+			font-weight: 500;
+			font-size: 14px;
+			opacity: 0;
+		}
+
+
+		.imageWrapper-38T7d9:hover .thumbQuicksave {
+			opacity: 0.8;
+		}
+
+		.thumbQuicksave:hover {
+			opacity: 1 !important;
+		}
+	`);
 
 	this.namingMethods = {
 		// You can add your own naming methods easily, just copy that function 
@@ -85,6 +117,7 @@ Quicksave.prototype.start = function () {
 };
 
 Quicksave.prototype.stop = function () {
+	BdApi.clearCSS("quicksave-style");
 };
 
 Quicksave.prototype.accessSync = function(dir){
@@ -102,6 +135,7 @@ Quicksave.prototype.observer = function (e) {
 	// MutationObserver, function is bound by BetterDiscord.
 	// We use this to see if user opens an image, if so, add a button.
 
+	// IMAGE OPEN BUTTON
 	if(e.addedNodes.length > 0 && e.addedNodes[0].className=='backdrop-2ohBEd'){
 
 		var settings = this.loadSettings();
@@ -115,20 +149,21 @@ Quicksave.prototype.observer = function (e) {
 		if(!elem) return;
 
 		var button = document.createElement('a');
+
+		// This class gives the styling of the Open Original buttom
+		// These will break every other update now it seems
+		button.className = "downloadLink-wANcd8 size14-1wjlWP weightMedium-13x9Y8"; 
+
 		fs.access(settings.direcotry, fs.W_OK, err=>{
 			if (err){
 				button.id = "qs_button";
 				
-				button.className = "download-button";
 				button.innerHTML = "Can't Quicksave: save location is invalid";
 			}else{
 				button.id = "qs_button";
 				button.href = "#";
 				button.onclick = this.saveCurrentImage.bind(this);
 
-				// This class gives the styling of the Open Original buttom
-				// These will break every other update now it seems
-				button.className = "downloadLink-wANcd8 size14-1wjlWP weightMedium-13x9Y8"; 
 
 				button.style.paddingLeft = "10px";
 
@@ -139,6 +174,35 @@ Quicksave.prototype.observer = function (e) {
 
 		});
 	}
+
+	// THUMBNAIL BUTTON
+	for(let i = 0; i < e.addedNodes.length; i++){
+
+
+		if(e.addedNodes[i].localName != "img") continue;
+		if(!e.addedNodes[i].parentElement.classList.contains('imageWrapper-38T7d9')) continue;
+
+		console.log('SHOULD APPEND', e.addedNodes[i]);
+		let div = document.createElement('div');
+
+		div.innerHTML = "Save";
+
+		div.className = "thumbQuicksave";
+
+		div.onclick = (e)=>{
+			// Prevent parent from opening the image
+			e.stopPropagation();
+			e.preventDefault();
+
+			this.saveThumbImage(e);
+		};
+
+		// appendChild but as the first child
+		e.addedNodes[i].parentElement.insertAdjacentElement('afterbegin', div);
+
+
+	}
+
 	
 };
 Quicksave.prototype.saveSettings = function (button) {
@@ -262,33 +326,103 @@ Quicksave.prototype.randomFilename64 = function(length){
 };
 
 
-Quicksave.prototype.saveCurrentImage = function(){
-	var button = document.getElementById('qs_button');
-	button.innerHTML = "Downloading...";
-	var plugin = BdApi.getPlugin('Quicksave');
-	var settings = plugin.loadSettings();
-	var fs = require('fs');
-	var dir = settings.direcotry;
 
+// Only purpoise of the next two functions is to interact with the DOM! 
+// These will be called when user clicks on save image buttons.
+// Extract the url, run download() and display info to user here.
+Quicksave.prototype.saveCurrentImage = function(){
+	// Called from the modal
+	var button = document.getElementById('qs_button');
+	var plugin = BdApi.getPlugin('Quicksave');
 
 	var url = document.querySelector(
 		'div.modal-2LIEKY > div > div > div:nth-child(2) > a:nth-child(1)'
 	).attributes[0].nodeValue;
 
+	if(!url) {
+		button.innerHTML = "Error: Couldn't extract url!";
+		return;
+	}
+
+	button.innerHTML = "Downloading...";
+
+	plugin.download(url, (err)=>{
+		if(err) 
+			button.innerHTML = "Error! " + err;
+		else
+			button.innerHTML = "Download finished!";
+	}, 
+
+	(bytes, total) => {
+		button.innerHTML = `${bytes}b/${total}b (${(bytes/total*100)|0}%)`;
+	});
+};
+
+Quicksave.prototype.saveThumbImage = function(e){
+	// Reimplementation of pull #2, icon in thumbnails
+	var button = e.srcElement;
+	var plugin = BdApi.getPlugin('Quicksave');
+
+
+	var url = button.parentElement.href;
+
+	if(!url) {
+		button.innerHTML = "Error";
+		console.error("Couldn't extract url!");
+		return;
+	}
+
+	button.innerHTML = "Wait";
+
+	plugin.download(url, (err)=>{
+		if(err) {
+			button.innerHTML = "Error";
+			console.error("Quicksave error: "+err);
+		}
+		else
+			button.innerHTML = "Done";
+	}, 
+
+	(bytes, total) => {
+		button.innerHTML = `${(bytes/total*100)|0}%`;
+	});
+};
+
+
+
+
+Quicksave.prototype.download = function(url, callback, progressCallback){
+	// Decides the filename, downloads and writes the file
+	//
+	//	callback:
+	//		In case of errors, first argument will be a string describing the error.
+	//		If everything's fine, its gonna be null
+	//
+	//	progressCallback
+	//		First argument will be downloaded bytes, second is total bytes
+
+	var plugin = BdApi.getPlugin('Quicksave');
+	var settings = plugin.loadSettings();
+	var fs = require('fs');
+	var dir = settings.direcotry;
 
 	// Removes ":large" from twitter's image urls. Pulled feature.
 	var twitterFix = new RegExp(":large$");
 	if (twitterFix.test(url)) url = url.replace(twitterFix, '');	
 
-
+	// Using node apis lets us skip all security built to chrome. This is useful
+	// if we want to download from anywhere we want, and we do: Discords rehosts
+	// are usually compressed.
+	// Can https use http? is this seperation necessary?
 	var net = (url.split('//')[0]=='https:') ? require('https') : require('http');
 
-	var filename = plugin.namingMethods[settings.namingmethod](plugin, settings, url, dir);
+
+	var filename = plugin.namingMethods[ settings.namingmethod ](
+		plugin, settings, url, dir
+	);
 
 	if(filename === null){
-		button.innerHTML = 
-			'Error while trying to find a free filename!'+
-			'Check console for more details.';
+		callback("Error while trying to find a free filename!");
 		return;
 	}
 
@@ -298,30 +432,62 @@ Quicksave.prototype.saveCurrentImage = function(){
 	console.log('-->');
 	console.log(dest);
 
-	var file = fs.createWriteStream(dest);
-	net.get(url, function(response) {
-		response.pipe(file);
-		file.on('finish', function() {
-			console.log("Finished");
-			button.innerHTML = "Download finished";
-			file.close();
+
+	// TODO: 
+	// Check if the downloaded file is actually an image
+	// Make sure we dont download gigabytes
+	// Dont write odd filenames, i've had my windows get very confused
+
+	const req = net.request(url, (res) => {
+		// console.log('statusCode:', res.statusCode);
+		// console.log('headers:', res.headers);
+		console.log(`${res.statusCode} ${url}`);
+		console.log(res);
+
+		if(res.statusCode != 200) {
+			callback("Server responded with "+ res.statusCode);
+			return;
+		}
+
+
+		let contentType = res.headers["content-type"];
+
+		if( contentType.search('image') < 0 ) {
+			callback(`Content-type '${contentType}' is not an image.`);
+			return;
+		}
+
+		let total = res.headers["content-length"];
+		let bytes = 0;
+
+		// Keep the file fragmented until all chunks are downloaded, then 
+		// concat. The only reason to do this is the ease of implementation.
+
+		let chunks = [];
+
+		res.on('data', (d) => {
+			chunks.push(d);
+
+			bytes+=d.length;
+			if(progressCallback) progressCallback(bytes, total);
 		});
-	}).on('error', function(err) {
-		fs.unlink(dest); 
 
-		// User may have closed the image, the button is no longer visible.
-		// When this happens, use BetterDiscords alert function that does not
-		// work! What is Jiiks doing?
-		if(document.getElementById('qs_button')) 
-			button.innerHTML = "Error: " + err.message;
-		else
-			BdApi.getCore().alert(
-				'Quicksave Error', 
-				`Failed to download file ${url}\nError: `+ err.message
-			);
-
-		console.log(err.message);
-		file.close();
+		res.on('end', ()=> {
+			fs.writeFile(dest, Buffer.concat(chunks), (err)=>{
+				if(err){
+					console.error(err);
+					callback(err.message);
+				} else {
+					callback(null);
+				}
+			});
+		});
 	});
-	
+
+	req.on('error', (err) => {
+		console.error(err);
+		callback(err.message);
+	});
+	req.end();
 };
+
